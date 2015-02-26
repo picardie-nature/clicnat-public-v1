babc6f49f32f3aa986664f0dfe24aa5487da109f
Nicolas Damiens <nicolas@damiens.info>
2015-02-26 16:36:31 +0100

    forcage de la mise a jour

--- scripts/maj-entrepot.php
+++ scripts/maj-entrepot.php
@@ -4,10 +4,11 @@ else require_once('/etc/baseobs/config.php');
 require_once(DB_INC_PHP);
 require_once(OBS_DIR.'espece.php');
 get_db($db);
+$forcage_mise_a_jour = true;
 foreach(bobs_espece::especes($db) as $e) {
 	echo "$e";
 	flush();
-	$e->enregistre_liste_communes_presence();
+	$e->enregistre_liste_communes_presence($forcage_mise_a_jour);
 	echo " \n";
 }
 bobs_espece::entrepot_calcul_stats_communes();
